04850dd2c345f7903c2b35fa3fffdd0335c07aa7
Mumtozmahal Salimova <m.salimova@newuu.uz>
2024-09-17 16:35:40 +0500

    up

--- student/Homework1.java
+++ student/Homework1.java
@@ -36,12 +36,79 @@ public class Homework1 {
     }
 
     public  int hex2int(String hex) {
-        //TODO Write your code here for problem 4
-        return 0;
+        switch(hex){
+            case "0":
+            return 0;
+            case "1":
+            return 1;
+            case "2":
+            return 2;
+            case "3":
+            return 3;
+            case "4":
+            return 4;
+            case "5":
+            return 5;
+            case "6":
+            return 6;
+            case "7":
+            return 7;
+            case "8":
+            return 8;
+            case "9":
+            return 9;
+            case "A":
+            return 10;
+            case "B":
+            return 11;
+            case "C":
+            return 12;
+            case "D":
+            return 13;
+            case "E":
+            return 14;
+            case "F":
+            return 15;
+            }
+        return -1;
     }
     public String int2hex(int value) {
-        //TODO Write your code here for problem 4
-        return "";
+        switch(value){
+            case 0:
+            return "0";
+            case 1:
+            return "1";
+            case 2:
+            return "2";
+            case 3:
+            return "3";
+            case 4:
+            return "4";
+            case 5:
+            return "5";
+            case 6:
+            return "6";
+            case 7:
+            return "7";
+            case 8:
+            return "8";
+            case 9:
+            return "9";
+            case 10:
+            return "A";
+            case 11:
+            return "B";
+            case 12:
+            return "C";
+            case 13:
+            return "D";
+            case 14:
+            return "E";
+            case 15:
+            return "F";
+        }
+
+        return "-1";
     }
 
 
