49626391b8f61529368f9d94df2c6ba7bd45508a
mumtoz <140655398+smumt@users.noreply.github.com>
2024-09-17 12:24:52 +0500

    Initialize master

---
+++ src/main/java/org/example/Problem2.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class Problem2 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/org/example/Problem3.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class Problem3 {
+    public static void main(String[] args) {
+
+    }
+}
