659dbc3a02971526afcdc825544524bbac56cb7c
Mumtozmahal Salimova <m.salimova@newuu.uz>
2024-09-17 16:41:04 +0500

    up1

--- student/Homework1.java
+++ student/Homework1.java
@@ -69,6 +69,18 @@ public class Homework1 {
             return 14;
             case "F":
             return 15;
+            case "a":
+            return 10;
+            case "b":
+            return 11;
+            case "c":
+            return 12;
+            case "d":
+            return 13;
+            case "e":
+            return 14;
+            case "f":
+            return 15;
             }
         return -1;
     }
